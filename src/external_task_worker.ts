31c28bcb80341753436fd013e19402c3046da93f
ElRaptorus <christian.werner@5minds.de>
2018-10-11 16:44:32 +0200

    :recycle: Remove invocation handling from sample worker It now just uses a simple sample result.

--- src/external_task_worker.ts
+++ src/external_task_worker.ts
@@ -1,7 +1,6 @@
 import * as bluebird from 'bluebird';
 import {Logger} from 'loggerhythm';
 
-import {HttpClient} from '@essential-projects/http';
 import {IIdentity} from '@essential-projects/iam_contracts';
 
 import {ExternalTask, IExternalTaskApi} from '@process-engine/external_task_api_contracts';
@@ -18,11 +17,10 @@ export class ExternalTaskSampleWorker {
   public config: any;
 
   private _externalTaskApiClient: IExternalTaskApi;
-  private _httpClient: HttpClient;
 
   private _intervalTimer: any;
 
-  private sampleIdentity: IIdentity = {
+  private _sampleIdentity: IIdentity = {
     token: 'defaultUser',
   };
 
@@ -30,13 +28,9 @@ export class ExternalTaskSampleWorker {
     this._externalTaskApiClient = externalTaskApiClient;
   }
 
-  public initialize(): void {
-    this._httpClient = new HttpClient();
-  }
-
-  public start<TPayloadType, TResultType>(): void {
+  public start<TPayload, TResult>(): void {
     this._intervalTimer = setInterval(async() => {
-      await this._fetchAndProcessExternalTasks<TPayloadType, TResultType>();
+      await this._fetchAndProcessExternalTasks<TPayload, TResult>();
     }, this.config.pollingInterval);
   }
 
@@ -52,12 +46,12 @@ export class ExternalTaskSampleWorker {
    *
    * @async
    */
-  private async _fetchAndProcessExternalTasks<TPayloadType, TResultType>(): Promise<void> {
+  private async _fetchAndProcessExternalTasks<TPayload, TResult>(): Promise<void> {
 
-    const availableExternalTasks: Array<ExternalTask<TPayloadType>> =
+    const availableExternalTasks: Array<ExternalTask<TPayload>> =
       await this
         ._externalTaskApiClient
-        .fetchAndLockExternalTasks<TPayloadType>(this.sampleIdentity,
+        .fetchAndLockExternalTasks<TPayload>(this._sampleIdentity,
                                                  this.config.workerId,
                                                  this.config.topicName,
                                                  this.config.maxTasks,
@@ -69,8 +63,8 @@ export class ExternalTaskSampleWorker {
 
       this.stop();
 
-      await bluebird.each(availableExternalTasks, async(externalTask: ExternalTask<TPayloadType>) => {
-        return this._processExternalTask<TPayloadType, TResultType>(externalTask);
+      await bluebird.each(availableExternalTasks, async(externalTask: ExternalTask<TPayload>) => {
+        return this._processExternalTask<TPayload, TResult>(externalTask);
       });
 
       logger.info('All tasks processed.');
@@ -83,95 +77,30 @@ export class ExternalTaskSampleWorker {
    *
    * @async
    */
-  private async _processExternalTask<TPayloadType, TResultType>(externalTask: ExternalTask<TPayloadType>): Promise<void> {
+  private async _processExternalTask<TPayload, TResult>(externalTask: ExternalTask<TPayload>): Promise<void> {
 
     logger.info(`Processing ExternalTask ${externalTask.id}.`);
-    const externalTaskInvocation: Model.Activities.ExternalTaskInvocation = this._parseInvocation(externalTask.payload);
 
-    if (externalTaskInvocation) {
-      try {
-        logger.info('Invocation attached to ExternalTask: ', externalTaskInvocation);
+    const result: TResult = await this._getSampleResult(externalTask.payload);
 
-        const result: TResultType = await this._executeInvocation<TResultType>(externalTaskInvocation);
+    await this._externalTaskApiClient.finishExternalTask<TResult>(this._sampleIdentity, this.config.workerId, externalTask.id, result);
 
-        await this._externalTaskApiClient.finishExternalTask<TResultType>(this.sampleIdentity, this.config.workerId, externalTask.id, result);
+    logger.info(`Finished processing ExternalTask with ID ${externalTask.id}.`);
 
-        logger.info(`Finished processing ExternalTask with ID ${externalTask.id}.`);
-      } catch (error) {
-        const message: string = 'Failed to execute ExternalTask!';
-        logger.error(message, error);
-
-        await this
-          ._externalTaskApiClient
-          .handleServiceError(this.sampleIdentity, this.config.workerId, externalTask.id, message, JSON.stringify(error));
-      }
-    } else {
-      const notSupportedError: string = `Invalid job configuration for ExternalTask with ID ${externalTask.id}`;
-      logger.error(notSupportedError, externalTaskInvocation);
-
-      await this
-        ._externalTaskApiClient
-        .handleServiceError(this.sampleIdentity, this.config.workerId, externalTask.id, notSupportedError, JSON.stringify(externalTask.payload));
-    }
   }
 
   /**
-   * Tries to parse the given data object into an ExternalTaskInvocation.
-   * If some required properties are missing, an empty object is returned.
+   * Returns some sample result with which to finish the ExternalTasks.
    *
-   * @param   data The data object from which to create an invocation.
-   * @returns      The parsed invocation.
+   * @returns The sample result.
    */
-  private _parseInvocation(data: any): Model.Activities.ExternalTaskInvocation {
+  private _getSampleResult(payload: any): any {
 
-    const urlProperty: string = data['url'];
-    const methodProperty: string = data['method'];
-    const headersProperty: string = data['headers'];
-    const payloadProperty: string = data['payload'];
+    const sampleResult: any = {
+      testResults: payload,
+    };
 
-    if (!(urlProperty && methodProperty)) {
-      return undefined;
-    }
-
-    const externalTaskInvocation: Model.Activities.ExternalTaskInvocation = new Model.Activities.ExternalTaskInvocation();
-
-    externalTaskInvocation.url = urlProperty;
-    externalTaskInvocation.method = methodProperty;
-    externalTaskInvocation.headers = headersProperty ? headersProperty : '{}';
-    externalTaskInvocation.payload = payloadProperty ? payloadProperty : '{}';
-
-    return externalTaskInvocation;
-  }
-
-  /**
-   * Uses the HTTP client to execute the given ExternalTaskInvocation.
-   *
-   * @async
-   * @param   invocation The invocation to execute.
-   * @returns            The result of the HTTP request.
-   */
-  private async _executeInvocation<TResultType>(invocation: Model.Activities.ExternalTaskInvocation): Promise<TResultType> {
-
-    const method: string = invocation.method.toLowerCase();
-
-    const options: any = {};
-
-    if (invocation.headers) {
-      options.headers = JSON.parse(invocation.headers);
-    }
-
-    let result: any;
-
-    logger.info('Calling: ', invocation.url);
-    if (method === 'get') {
-      result = await this._httpClient.get<TResultType>(invocation.url, options);
-    } else {
-      result = await this._httpClient[method](invocation.url, invocation.payload, options);
-    }
-
-    logger.info('Received the following response: ', result);
-
-    return <TResultType> result;
+    return sampleResult;
   }
 
 }
